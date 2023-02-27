4e2b13b25ae380261c857cc0800f9037c95db961
Anders Jonsson <aj224rj@student.lnu.se>
2023-02-27 11:14:03 +0100

    Add folder-structure with controllers and dummy-views for needed views

---
+++ Assignment Wt1 Oauth/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_Wt1_Oauth.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("[controller]/{id}")]
+        public IActionResult Index(string id)
+        {
+            ViewBag.userid = id;
+            return View(ViewBag);
+        }
+
+        [Route("[controller]/{id}/activities")]
+        public IActionResult Activities(string id)
+        {
+            return View();
+        }
+
+        [Route("[controller]/{id}/groups")]
+        public IActionResult Groups(string id)
+        {
+            return View();
+        }
+    }
+}
