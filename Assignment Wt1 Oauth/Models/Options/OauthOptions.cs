7de63dd33448324df2f9f86f94bcc10f8e7eab2a
Anders Jonsson <aj224rj@student.lnu.se>
2023-02-27 13:02:25 +0100

    Add options model for oauth options

---
+++ Assignment Wt1 Oauth/Models/Options/OauthOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment_Wt1_Oauth.Models.Options
+{
+    public class OauthOptions
+    {
+        public string AppId { get; set; }
+        public string RedirectUri { get; set; }
+        public string State { get; set; }
+        public string RequestedScopes { get; set; }
+        public string AppSecret { get; set; }
+        public string Code { get; set; }
+
+        public OauthOptions(string appId, string redirectUri, string state, string requestedScopes, string appSecret, string code)
+        {
+            AppId = appId;
+            RedirectUri = redirectUri;
+            State = state;
+            RequestedScopes = requestedScopes;
+            AppSecret = appSecret;
+            Code = code;
+        }
+    }
+}
